6ddafb14c2cbe4cba7d726da1b629e28fadafc76
Max <mzdesa@icloud.com>
2022-12-03 13:09:03 -0800

    First commit

---
+++ communication/communication.ino
@@ -0,0 +1,18 @@
+/*
+ * This file contains a class used for communication between two ESP32s.
+ * It contains utilities for encoding and decoding different pieces of data
+ * according to a binary communication protocol.
+ */
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
